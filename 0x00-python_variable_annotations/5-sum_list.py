2f9f298a55ee28ce32f1d0975bcf081ee14d0657
Faith-qa <atienofaith12@gmail.com>
2021-07-24 06:57:24 +0300

    sum of a list

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import typing
+
+"""
+takes a list of floats ant returns
+its sum
+"""
+
+
+def sum_list(input_list: typing.List[float]):
+    """ takes a list of floats as arguments and returns their sum"""
+    return sum(input_list)
